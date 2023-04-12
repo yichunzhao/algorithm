63f35a176d5fa13067919c82018ae570d56c32c0
wenqi0425 <xiaoc288@gmail.com>
2023-04-12 08:08:00 +0200

    updating comment

--- recursion/src/main/java/com/ynz/smart/recursion/DifferenceOfPerformance.java
+++ recursion/src/main/java/com/ynz/smart/recursion/DifferenceOfPerformance.java
@@ -4,6 +4,11 @@ package com.ynz.smart.recursion;
  * the following experience shows that
  * 1) recursion do give 40 times better performance than loop.
  * 2) but it may cause stackoverflow when a big input.
+ * <p>
+ * In Java, tail call optimization is not guaranteed by the language specification.
+ * <p>
+ * This means that, in general, tail-recursive functions in Java are not automatically optimized to use a
+ * constant amount of memory, and may still result in a stack overflow error for large input values.
  */
 public class DifferenceOfPerformance {
     public static final int MILLION = 10_000;
