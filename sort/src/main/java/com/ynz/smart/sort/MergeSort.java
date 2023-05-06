e16fa050881cc58f88b069b6f3467f9ad35ad620
wenqi0425 <xiaoc288@gmail.com>
2023-05-07 00:50:45 +0200

    merge sorting

---
+++ sort/src/main/java/com/ynz/smart/sort/MergeSort.java
@@ -0,0 +1,78 @@
+package com.ynz.smart.sort;
+
+import java.util.Arrays;
+
+/**
+ * time complexity 0(n*long(n)), rather than O(n*n)
+ * space complexity is higher than others(bubble, insert, selection), they sort in place.
+ */
+public class MergeSort extends Base {
+
+    public static void main(String[] args) {
+        System.out.println("unsorted array:" + Arrays.toString(numbs));
+        //sort the array using the merge sort.
+        mergeSort(numbs);
+        System.out.println("sorted array:" + Arrays.toString(numbs));
+
+    }
+
+    public static void mergeSort(int[] inputArray) {
+        // recursive base case: until a single element
+        int length = inputArray.length;
+        if (length <= 1) {
+            return;
+        }
+
+        // middle point of input array,
+        int middle = length / 2;
+
+        // divide problem into two small problems
+        // Split the original array into left and right arrays
+        int[] leftArray = Arrays.copyOfRange(inputArray, 0, middle);
+        int[] rightArray = Arrays.copyOfRange(inputArray, middle, inputArray.length);
+
+        // Print the left and right arrays
+        System.out.println("Left array: " + Arrays.toString(leftArray));
+        System.out.println("Right array: " + Arrays.toString(rightArray));
+
+        // solving two small problems respectively.
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray, rightArray, inputArray);
+    }
+
+    private static void merge(int[] leftArray, int[] rightArray, int[] inputArray) {
+        System.out.println("merge inputArray size:" + inputArray.length);
+        int leftArraySize = inputArray.length / 2;
+        int rightArraySize = inputArray.length - leftArraySize;
+
+        int i = 0;
+        int l = 0;
+        int r = 0;
+
+        while (l < leftArraySize && r < rightArraySize) {
+            if (leftArray[l] < rightArray[r]) {
+                inputArray[i] = leftArray[l];
+                i++;
+                l++;
+
+            } else {
+                inputArray[i] = rightArray[r];
+                i++;
+                r++;
+            }
+        }
+        //maybe one element left, then we need handle it
+        while (l < leftArraySize) {
+            inputArray[i] = leftArray[l];
+            i++;
+            l++;
+        }
+
+        while (r < rightArraySize) {
+            inputArray[i] = rightArray[r];
+            i++;
+            r++;
+        }
+    }
+}
